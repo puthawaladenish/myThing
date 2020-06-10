e2520090ab08bd91cb4b49c12a01b3624574df5b
Denish Puthawala <puthawala.denish@gmail.com>
2020-06-10 13:29:39 +0530

    ok

--- src/components/Maininfo.js
+++ src/components/Maininfo.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 
 class Maininfo extends React.Component {
-    render() {
+    render() { 
+        const info = require('../pics/info.gif');
+        
         return (
             <div class="row middlebg" >
                 <div class="col-sm-6 sideinfo">
                     <p> A social platform where people can express what they are use.</p>
-                    <p>Simpally register enter your your thing's info, get your public link and put your social media platform's info section</p>
+                    {/*<img src={phone}  alt="Phone" width="50" height="50"/><br/>
+                    <img src={laptop}  alt="Phone" width="50" height="50"/><br/>
+                    <img src={pc}  alt="Phone" width="50" height="50"/><br/>
+                    <img src={camera}  alt="Phone" width="50" height="50"/><br/>
+                    <img src={watch}  alt="Phone" width="50" height="50"/><br/>
+        <img src={car}  alt="Phone" width="50" height="50"/><br/>*/}
+                    <img src={info}  alt="Phone" width="500" height="500"/><br/>
 
+                    <p>Simpally register enter your your thing's info, get your public link and put your social media platform's info section</p>  
                 </div>
                 <div class="col-sm-6" >
                     <form action="">
